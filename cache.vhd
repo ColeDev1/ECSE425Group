15f26ffad187f6050cce0469afb6400a871da84a
Cole <dontdont1234@gmail.com>
2026-02-15 16:03:27 -0500

    Added more states to represent hits and misses for reads and writes

--- cache.vhd
+++ cache.vhd
@@ -28,7 +28,7 @@ port(
 end cache;
 
 architecture arch of cache is
-type state_type is (IDLE,READING,READ_READY,WRITING);
+type state_type is (IDLE,READING,READ_READY,WRITING,READ_MISS,READ_HIT,WRITE_MISS,WRITE_HIT,WRITE_MEM,READ_MEM);
 signal state: state_type;
 signal next_state: state_type;
 -- declare signals here
@@ -50,48 +50,74 @@ avalon_structure_proc : process (clock)
 
 	case state is
 		when IDLE =>
-		
-			if s_read = '1' and  then
+			if s_read = '1' then 
 				next_state <= READING;
-				m_read <= '1';
-				--set up for memory / cache check
-				
 			elsif s_write = '1' then
-				next_state <= WRITING;
-				m_write <= '1';
-				--set up for write
-			else	
+				next_state <=WRITING;
+			else
 				next_state <= IDLE;
 			end if;
-			
-		when READING =>
-			--here we need to check valid and dirty bit
-			--cache check first before memory
-			if m_waitrequest = '0' then
-				next_state <= READ_READY;
-				s_waitrequest <= '0';
-				--set s_waitrequest to 0
+		when READING =>			
+			if valid = '1' and match then
+				next_state <= READ_HIT;
 			else	
-				next_state <= READING;
-				
+				next_state <= READ_MISS;
 			end if;
+		when READ_HIT =>
+			--this state is just reading from cache
+			next_state <= READ_READY;
+			s_readdata <= ... memory array here;
+			s_waitrequest <= '0';
+			
+		when READ_MISS =>
+			--this state is setting up 16 memory accesses
+			--figure out where we are in realtion to the 16 bytes in a cache line // block
+			--set the addr to the first byte then in read mem we just need to do 16 sequential reads 
+		when READ_MEM =>
+			--this state is 16 accesses
+			--then place them in cache
+			if memory access is complete then 
+				next_state <= READ_HIT;
+				s_addr <= needs to be reset to what it was before
+			else
+				next_state <= READ_MEM;
+			end if;
+			--complete 16 sequential byte accesses in memory
+			--could be for loop here to access the 16 bytes
 			
 		when READ_READY =>
-			
+			--this state is to create a 1 clock cycle buffer to read the read data
 			next_state <= IDLE;
-			s_waitrequest <= '1';
-			
-			
+			s_waitrequest <='1'; --maybe need to put somewhere else
+		
 		when WRITING =>
 			--check for tag match
-			if m_waitrequest = '1' then
-				next_state <= WRITING;
+			if Tag and Valid then
+				next_state <= WRITE_HIT;
 			else	
-				next_state <= IDLE;
-				s_waitrequest => '0';
+				next_state <= WRITE_MISS;
 			end if;
-		when others =>
-			next_state => IDLE;
+		
+		when WRITE_HIT =>
+			--write to cache 
+			--set dirty bit to 1
+			
+		when WRITE_MISS =>
+			--this state to set up memory access
+			if v = '1' and d = '1' then 
+				write to memory;
+				if m_waitrequest = '0' then 
+					next_state <= WRITE_MEM;
+				else 
+					next_state <= WRITE_MISS;
+				end if;
+			else
+				next_state <= WRITE_MEM;
+			end if;
+			
+		when WRITE_MEM =>
+			-- must read 16 bytes and then write our new byte into the cache;
+		
+			
 	end case;
-
 end arch;
